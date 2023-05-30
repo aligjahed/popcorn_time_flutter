7f9997e436d758415e087e641b02d8c726fa3a8f
Ali G.Jahed <aligjahed@gmail.com>
2023-05-30 17:32:01 +0330

    Add Costume fonts and colros

---
+++ lib/config/Colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class Colors {
+  static final Color mainPurple = Color(0xFF5E3690);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 import 'db/database.dart';
 
@@ -24,6 +25,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Estedad'
       ),
       home: const HomePage(),
     );
@@ -35,6 +37,55 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return IntroductionScreen(
+      next: const Text('next' , style: TextStyle(color: Colors.black)),
+      skip: Text('Skip'),
+      back: Text('back'),
+      showBottomPart: true,
+      isProgressTap: false,
+      showBackButton: true,
+
+      pages: [
+        PageViewModel(
+          title: "Title of orange text and bold page",
+          body: "This is a description on a page with an orange title and bold, big body.",
+          image: const Center(
+            child: Text("👋", style: TextStyle(fontSize: 100.0)),
+          ),
+          decoration: const PageDecoration(
+            titleTextStyle: TextStyle(color: Colors.orange),
+            bodyTextStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 20.0),
+          ),
+        ),
+        PageViewModel(
+          title: "Title of custom button page",
+          body: "This is a description on a page with a custom button below.",
+          image: Image.asset("res/images/logo.png", height: 175.0),
+          footer: ElevatedButton(
+            onPressed: () {
+              // On button pressed
+            },
+            child: const Text("Let's Go!"),
+          ),
+        ),
+        PageViewModel(
+          title: "Title of custom body page",
+          bodyWidget: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Click on "),
+              Icon(Icons.edit),
+              Text(" to edit a post"),
+            ],
+          ),
+          image: const Center(child: Icon(Icons.android)),
+        )
+      ],
+      showNextButton: false,
+      done: const Text("Done"),
+      onDone: () {
+        // On button pressed
+      },
+    );
   }
 }
